55e41b1fbbc8df6f508b05cdc8a1db5a107727b1
kmzink1079 <kevin_znk@yahoo.com>
2019-09-18 19:08:57 -0400

    Weeks 1 - 4 are updated aside from CSS.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Button AddProductButton;
+
+    @FXML
+    private Button RecordProductionButton;
+
+    @FXML
+    void AddProductBtnPushed(ActionEvent event) {
+    System.out.println("AddProduct");
+    }
+
+    @FXML
+    void recordProductionButtonPushed(ActionEvent event) {
+    System.out.println("product Recorded");
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * @author Kevin Zink
+ * @brief
+ */
 public class Main extends Application {
+  /**
+   * The starting point of a javaFX program.
+   *
+   * @param primaryStage the first thing the use sees, primary stage of the javaFX app
+   * @throws Exception
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Hello World");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
+    String str = "string";
+    String substr = str.substring(0);
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
